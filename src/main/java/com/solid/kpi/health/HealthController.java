ca41bb104dfd390baacfcb6232f618f8c7ca15b6
digitpic <jkw5033@gmail.com>
2025-01-11 21:00:08 +0900

    feat(health): Add health check feature

---
+++ src/main/java/com/solid/kpi/health/HealthController.java
@@ -0,0 +1,13 @@
+package com.solid.kpi.health;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.status(200).body("ok");
+    }
+}
